e98fd6f0a5c8cc1e8e941dd22acc286396577999
JKielczynska <joannapelczynska@gmail.com>
2017-12-11 12:50:43 +0100

    14.4 Add @RequestBody and @PathVariable

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,9 +1,12 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.TaskDto;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,22 +21,22 @@ public class TaskController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "{taskId}")
-    public TaskDto getTask(String taskId) {
-        return new TaskDto ((long)1, "test title", "test_content");
+    public TaskDto getTask(@PathVariable final String taskId) {
+        return new TaskDto((long)1, "test title", "test_content");
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "{taskId}")
-    public void deleteTask(String taskId) {
+    public void deleteTask(@PathVariable final String taskId) {
 
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "{taskId}")
-    public TaskDto updateTask(TaskDto taskDto) {
+    public TaskDto updateTask(@RequestBody final TaskDto taskDto) {
         return new TaskDto((long)1, "Edited test title", "Test content");
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public void createTask(TaskDto taskDto) {
+    public void createTask(@RequestBody final TaskDto taskDto) {
 
     }
 }
