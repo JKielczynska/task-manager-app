62872ec215b7d5a842e8ed6f93ac494ad5058b57
JKielczynska <joannapelczynska@gmail.com>
2018-01-16 14:01:04 +0100

    19.3 inversion of the condition in createMailMessage()

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -33,11 +33,11 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if (!mail.getToCc().isEmpty()) {
+        if (mail.getToCc().isEmpty()) {
+            LOGGER.info("CC is not included.");
+        } else {
             mailMessage.setCc(mail.getToCc());
             LOGGER.info("CC added.");
-        } else {
-            LOGGER.info("CC is not included.");
         }
         return mailMessage;
     }
