f0b087d2c15f719a596927a2d6b33416dcdc5a49
JKielczynska <joannapelczynska@gmail.com>
2018-01-18 17:01:42 +0100

    19.4 remove @Override in TaskRepository interface

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -8,18 +8,13 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
-    @Override
     List<Task> findAll();
 
-    @Override
     Optional<Task> findById(Long id);
 
-    @Override
     Task save(Task task);
 
-    @Override
     void deleteById(Long id);
 
-    @Override
     long count();
 }
